867ab1c5b8aca2fc72a18aff2aa466da1cfef5a2
rquiq <31048909+rquiq@users.noreply.github.com>
2018-11-11 21:10:42 +0100

    crypt-file
    crypt file with Caser Cypher and key using python

---
+++ CryptDcryptFile.py
@@ -0,0 +1,45 @@
+def crypt():
+    alphabet = 'abcdefghijklmnopqrstuvwxyz' 
+    key = int(input('Enter a key : '))
+    Nmessage = ''
+
+    fileName = input('Enter a file name : ')
+    message = open(fileName).read()
+
+    for ch in message:
+        if ch in alphabet:
+            position = alphabet.find(ch)
+            Nposition = (position + key )%26
+            Nch = alphabet[Nposition]
+
+            Nmessage += Nch
+        else:
+            Nmessage += ch
+
+    print(Nmessage)
+    file = open(fileName,'w')
+    file.write(Nmessage)
+    file.close()
+
+def dcrypt():
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    key = int(input('Enter a key : '))
+    Nmessage = ''
+
+    fileName = input('Enter a file name : ')
+    message = open(fileName).read()
+
+    for ch in message:
+        if ch in alphabet:
+            position = alphabet.find(ch)
+            Nposition = (position - key )%26
+            Nch = alphabet[Nposition]
+
+            Nmessage += Nch
+        else:
+            Nmessage += ch
+
+    print(Nmessage)
+    file = open(fileName,'w')
+    file.write(Nmessage)
+    file.close()
